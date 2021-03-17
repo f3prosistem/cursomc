b4c92069bb758a4c3f5c8122f2e6aa329c7ef370
Francivaldo Souza <francivaldo.alves@gmail.com>
2021-03-17 19:26:42 -0300

    Projeto criado

---
+++ src/test/java/com/f3pro/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f3pro.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
