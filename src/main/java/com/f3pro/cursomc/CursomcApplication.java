8f5a51e2ea6e93e328293263735afec470df76f4
Francivaldo Souza <francivaldo.alves@gmail.com>
2021-03-18 21:20:15 -0300

    Produtos e associacao muitos para muitos

--- src/main/java/com/f3pro/cursomc/CursomcApplication.java
+++ src/main/java/com/f3pro/cursomc/CursomcApplication.java
@@ -1,7 +1,9 @@
 package com.f3pro.cursomc;
 
 import com.f3pro.cursomc.domain.Categoria;
+import com.f3pro.cursomc.domain.Produto;
 import com.f3pro.cursomc.repositories.CategoriaRepository;
+import com.f3pro.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,6 +17,8 @@ public class CursomcApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -23,10 +27,19 @@ public class CursomcApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1= new Categoria(null,"Informatica");
 		Categoria cat2= new Categoria(null,"Escritorio");
-		Categoria cat3= new Categoria(null,"Livros");
 
+		Produto p1 = new Produto(null,"Computador",2000.00);
+		Produto p2= new Produto(null,"Impressora",800.00);
+		Produto p3 = new Produto(null,"Mouse",80.00);
 
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 
 	}
 }
--- src/main/java/com/f3pro/cursomc/domain/Categoria.java
+++ src/main/java/com/f3pro/cursomc/domain/Categoria.java
@@ -1,11 +1,12 @@
 package com.f3pro.cursomc.domain;
 
-import java.io.Serializable;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
 
 @Entity
 public class Categoria implements Serializable {
@@ -16,6 +17,9 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY )
 	private  Integer id;
 	private String nome;
+	@JsonIgnore
+@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
 		
@@ -41,6 +45,14 @@ public class Categoria implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 
 	@Override
