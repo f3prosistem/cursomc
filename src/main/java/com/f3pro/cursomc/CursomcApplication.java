6e6ba654282f8cedee728fd3577869a89c58bbd8
francivaldo <f3pro@pop-os.localdomain>
2021-03-29 20:49:23 -0300

    paginação com parametros opcionais na requisição

--- src/main/java/com/f3pro/cursomc/CursomcApplication.java
+++ src/main/java/com/f3pro/cursomc/CursomcApplication.java
@@ -66,6 +66,11 @@ public class CursomcApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
+		Categoria cat3 = new Categoria(null, "Cama mesa e banho");
+		Categoria cat4= new Categoria(null, "jardinagem");
+		Categoria cat5 = new Categoria(null, "Decoração");
+		Categoria cat6 = new Categoria(null, "eletronicos");
+		Categoria cat7 = new Categoria(null, "Pefumes");
 
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -79,7 +84,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
 		// Populando Categoria e Produto
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3,cat4,cat5,cat6,cat7));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
 		Estado est1 = new Estado(null, "Minas Gerais");
--- src/main/java/com/f3pro/cursomc/services/CategoriaService.java
+++ src/main/java/com/f3pro/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.f3pro.cursomc.domain.Categoria;
@@ -15,40 +18,46 @@ import com.f3pro.cursomc.services.exeptions.ObjectNotFoundException;
 @Service
 public class CategoriaService {
 
-	@Autowired
-	private CategoriaRepository repository;
+    @Autowired
+    private CategoriaRepository repository;
 // servico de consulta por  ID
 
-	public Categoria find(Integer id) {
-		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-	}
+    public Categoria find(Integer id) {
+        Optional<Categoria> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+    }
 
-	public Categoria insert(Categoria obj) {
-		obj.setId(null);// garantindo que vai ser um novo objeto inserido
-		return repository.save(obj);
-	}
+    public Categoria insert(Categoria obj) {
+        obj.setId(null);// garantindo que vai ser um novo objeto inserido
+        return repository.save(obj);
+    }
 
-	// atualizar a categoria
-	public Categoria update(Categoria obj) {
-		find(obj.getId());// valindado se existe a Id categoria
-		return repository.save(obj);
-	}
+    // atualizar a categoria
+    public Categoria update(Categoria obj) {
+        find(obj.getId());// valindado se existe a Id categoria
+        return repository.save(obj);
+    }
 
-	// serviço para deletar
-	public void delete(Integer id) {
-		find(id);
-		try {
-			repository.deleteById(id);
-		} catch (DataIntegrityViolationException e) {
+    // serviço para deletar
+    public void delete(Integer id) {
+        find(id);
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
 
-			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
-		}
-	}
+            throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+        }
+    }
 
-	public List<Categoria> findAll() {
-		return repository.findAll();
-	}
+    public List<Categoria> findAll() {
+        return repository.findAll();
+    }
+
+    //paginação de categoria
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return repository.findAll(pageRequest);
+    }
 
 }
